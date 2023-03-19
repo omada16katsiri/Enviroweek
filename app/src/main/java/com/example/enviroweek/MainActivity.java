34d489536272f1878871ac8a5f40043e4ab5794c
geormerk1 <geormerk1@ee.duth.gr>
2023-03-19 22:35:48 +0200

    Version 0.3

--- app/src/main/java/com/example/enviroweek/MainActivity.java
+++ app/src/main/java/com/example/enviroweek/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.enviroweek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,16 +26,18 @@ public class MainActivity extends AppCompatActivity
         Calendar calendar = Calendar.getInstance();
         int day = calendar.get(Calendar.DAY_OF_WEEK);
 
-        switch (day+1)
+        switch (day)
         {
             case Calendar.SUNDAY:
 
                 Sunday = (Button) findViewById(R.id.Sunday);
+                Sunday.setBackgroundColor(0xFF6200EE);
                 Sunday.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         openact("Κυριακή");
                         //openacttest();
+
                     }
 
                 });
@@ -42,6 +45,7 @@ public class MainActivity extends AppCompatActivity
                 break;
             case Calendar.MONDAY:
                 Monday = (Button) findViewById(R.id.Monday);
+                Monday.setBackgroundColor(0xFF6200EE);
                 Monday.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -51,6 +55,7 @@ public class MainActivity extends AppCompatActivity
                 break;
             case Calendar.TUESDAY:
                 Tuesday = (Button) findViewById(R.id.Tuesday);
+                Tuesday.setBackgroundColor(0xFF6200EE);
                 Tuesday.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -60,6 +65,7 @@ public class MainActivity extends AppCompatActivity
                 break;
             case Calendar.WEDNESDAY:
                 Wednesday = (Button) findViewById(R.id.Wednesday);
+                Wednesday.setBackgroundColor(0xFF6200EE);
                 Wednesday.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -69,6 +75,7 @@ public class MainActivity extends AppCompatActivity
                 break;
             case Calendar.THURSDAY:
                 Thursday = (Button) findViewById(R.id.Thursday);
+                Thursday.setBackgroundColor(0xFF6200EE);
                 Thursday.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -78,6 +85,7 @@ public class MainActivity extends AppCompatActivity
                 break;
             case Calendar.FRIDAY:
                 Friday = (Button) findViewById(R.id.Friday);
+                Friday.setBackgroundColor(0xFF6200EE);
                 Friday.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -87,6 +95,7 @@ public class MainActivity extends AppCompatActivity
                 break;
             case Calendar.SATURDAY:
                 Saturday = (Button) findViewById(R.id.Saturday);
+                Saturday.setBackgroundColor(0xFF6200EE);
                 Saturday.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -99,6 +108,8 @@ public class MainActivity extends AppCompatActivity
 
         }
     }
+
+
 
     protected void openact(String titles)
     {
